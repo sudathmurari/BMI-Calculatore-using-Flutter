348287d5f2b645ce55b3b14e2a797c6ecc16dec4
sudathmurari <sudathsudath@gmail.com>
2019-09-16 11:28:36 -0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/screens/input_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21)
+      ),
+      home: InputPage(),
+
+      
+    );
+  }
+}
